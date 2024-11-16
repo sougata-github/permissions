c04e96b1e6dc2c0003bb0af3500fd0560b8007a5
Sougata <sougatadas9874@gmail.com>
2024-11-16 12:56:07 +0530

    fix: typo in webhook enpoint.

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -40,7 +40,7 @@ export async function POST(req: Request) {
     case "user.created": {
       await clerk.users.updateUser(event.data.id, {
         publicMetadata: {
-          roles: ["user"],
+          role: ["user"],
         },
       });
       break;
