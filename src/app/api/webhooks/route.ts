557e1e4f372d7767799ede3143e850e50fd52b9b
Sougata <sougatadas9874@gmail.com>
2024-11-16 13:21:05 +0530

    change header names in webhook endpoint.

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -4,11 +4,11 @@ import { clerkClient, WebhookEvent } from "@clerk/nextjs/server";
 
 export async function POST(req: Request) {
   const headerPayload = await headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
+  const svixId = headerPayload.get("svix-id");
+  const svixTimestamp = headerPayload.get("svix-timestamp");
+  const svixSignature = headerPayload.get("svix-signature");
 
-  if (!svix_id || !svix_timestamp || !svix_signature) {
+  if (!svixId || !svixTimestamp || !svixSignature) {
     return new Response("Error: Missing Svix headers", {
       status: 400,
     });
@@ -23,9 +23,9 @@ export async function POST(req: Request) {
 
   try {
     event = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
+      "svix-id": svixId,
+      "svix-timestamp": svixTimestamp,
+      "svix-signature": svixSignature,
     }) as WebhookEvent;
   } catch (err) {
     console.error("Error: Could not verify webhook:", err);
